7cdc22517c8f68e50e7cce78da0e2266930ba233
Abel Fernandez <abel.fernandez@brainly.com>
2023-05-21 17:52:44 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by abel.fernandez on 21/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
