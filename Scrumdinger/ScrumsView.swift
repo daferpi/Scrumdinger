f279376c2404e47b3d4c9282ec1a9dd8f8d0d456
Abel Fernandez <abel.fernandez@brainly.com>
2023-05-22 08:31:04 +0200

    Create list view

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,25 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by abel.fernandez on 22/5/23.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums, id: \.title) { item in
+            CardView(scrum: item)
+                .listRowBackground(item.theme.mainColor)
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
